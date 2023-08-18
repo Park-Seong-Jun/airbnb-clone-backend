1f8bf1b0e8dd061b637e862bb9ea5408b5c2a1eb
Park-Seong-Jun <pasuju1021@naver.com>
2023-08-18 12:59:10 +0900

    basic page rendering

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rooms import urls
+from config.view import first
+from django.http import HttpResponse
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", first),
+    path("rooms/", include(urls)),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Room
+
 
 # Create your views here.
+
+
+def say_hello(request):
+    rooms = Room.objects.all()
+
+    return render(
+        request,
+        "see_all_rooms.html",
+        {
+            "rooms": rooms,
+            "title": "This page show all Rooms!",
+        },
+    )
+
+
+def see_room(request, room_id):
+    return HttpResponse(f"Room {room_id}")
